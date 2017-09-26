93248a41b5742a53d890d0354e9af13918b84130
sasonhe <642139009@qq.com>
2017-09-26 09:56:00 +0800

    add table

--- src/views/index.vue
+++ src/views/index.vue
@@ -63,14 +63,30 @@
 <div class="layout" :class="{'layout-hide-text': spanLeft < 5}">
     <Row type="flex">
         <Col :span="spanLeft" class="layout-menu-left">
-        <Menu active-name="1" theme="dark" width="auto">
+        <Menu active-name="1-1" theme="dark" width="auto" :open-names="['1']">
             <div class="layout-logo-left"></div>
-            <MenuItem name="1">
-            <Icon type="ios-navigate" :size="iconSize"></Icon> <span class="layout-text">选项 1</span> </MenuItem>
-            <MenuItem name="2">
-            <Icon type="ios-keypad" :size="iconSize"></Icon> <span class="layout-text">选项 2</span> </MenuItem>
-            <MenuItem name="3">
-            <Icon type="ios-analytics" :size="iconSize"></Icon> <span class="layout-text">选项 3</span> </MenuItem>
+            <Submenu name="1"> <template slot="title">
+                               <Icon type="ios-navigate"></Icon>
+                               导航一
+                           </template>
+                <MenuItem name="1-1">选项 1</MenuItem>
+                <MenuItem name="1-2">选项 2</MenuItem>
+                <MenuItem name="1-3">选项 3</MenuItem>
+            </Submenu>
+            <Submenu name="2"> <template slot="title">
+                               <Icon type="ios-keypad"></Icon>
+                               导航二
+                           </template>
+                <MenuItem name="2-1">选项 1</MenuItem>
+                <MenuItem name="2-2">选项 2</MenuItem>
+            </Submenu>
+            <Submenu name="3"> <template slot="title">
+                               <Icon type="ios-analytics"></Icon>
+                               导航三
+                           </template>
+                <MenuItem name="3-1">选项 1</MenuItem>
+                <MenuItem name="3-2">选项 2</MenuItem>
+            </Submenu>
         </Menu>
         </Col>
         <Col :span="spanRight">
@@ -87,7 +103,7 @@
         </div>
         <div class="layout-content">
             <div class="layout-content-main">
-                <Table></Table>
+                <Table-1></Table-1>
             </div>
         </div>
         <div class="layout-copy"> 2011-2016 &copy; TalkingData </div>
@@ -121,7 +137,7 @@ export default {
         }
     },
     components: {
-        Table
+        'Table-1': Table
     }
 }
 </script>
