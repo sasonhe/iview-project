3ba96a5f13eeaa9fa148f8934693b9aeacf06dc5
sasonhe <642139009@qq.com>
2017-09-27 10:03:50 +0800

    add

--- src/views/index.vue
+++ src/views/index.vue
@@ -65,25 +65,28 @@
         <Col :span="spanLeft" class="layout-menu-left">
         <Menu active-name="1-1" theme="dark" width="auto" :open-names="['1']">
             <div class="layout-logo-left"></div>
-            <Submenu name="1"> <template slot="title">
-                               <Icon type="ios-navigate"></Icon>
-                               导航一
-                           </template>
+            <Submenu name="1">
+                <template slot="title">
+                   <Icon type="ios-navigate"></Icon>
+                   导航一
+               </template>
                 <MenuItem name="1-1">选项 1</MenuItem>
                 <MenuItem name="1-2">选项 2</MenuItem>
                 <MenuItem name="1-3">选项 3</MenuItem>
             </Submenu>
-            <Submenu name="2"> <template slot="title">
-                               <Icon type="ios-keypad"></Icon>
-                               导航二
-                           </template>
+            <Submenu name="2">
+                <template slot="title">
+                   <Icon type="ios-keypad"></Icon>
+                   导航二
+               </template>
                 <MenuItem name="2-1">选项 1</MenuItem>
                 <MenuItem name="2-2">选项 2</MenuItem>
             </Submenu>
-            <Submenu name="3"> <template slot="title">
-                               <Icon type="ios-analytics"></Icon>
-                               导航三
-                           </template>
+            <Submenu name="3">
+                <template slot="title">
+                   <Icon type="ios-analytics"></Icon>
+                   导航三
+               </template>
                 <MenuItem name="3-1">选项 1</MenuItem>
                 <MenuItem name="3-2">选项 2</MenuItem>
             </Submenu>
@@ -92,8 +95,10 @@
         <Col :span="spanRight">
         <div class="layout-header">
             <Button type="text" @click="toggleClick">
-                        <Icon type="navicon" size="32"></Icon>
-                    </Button> </div>
+                <Icon type="navicon" size="32"></Icon>
+            </Button>
+
+        </div>
         <div class="layout-breadcrumb">
             <Breadcrumb>
                 <BreadcrumbItem href="#">首页</BreadcrumbItem>
